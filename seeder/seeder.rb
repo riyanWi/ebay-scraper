03a8f17c65294695acb8d36596b76566e8e849ed
Riyan Wibowo <tunggalrahayu1@gmail.com>
2022-02-28 21:30:23 +0700

    fix seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,5 +2,6 @@ pages << {
     page_type: 'listing',
     method: 'GET',
     headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-    url: 'https://www.ebay.com/b/Apple-iPhone/9355/bn_319682'
+    url: 'https://www.ebay.com/b/Apple-iPhone/9355/bn_319682',
+    fetch_type: "browser"
 }
