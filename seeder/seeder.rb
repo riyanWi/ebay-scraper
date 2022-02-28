9eb7a71ee23c4ee09a25e2d4600968c18160af45
Riyan Wibowo <tunggalrahayu1@gmail.com>
2022-02-28 21:18:16 +0700

    add config file

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,7 +1,7 @@
 pages << {
     page_type: 'listing',
     method: 'Get',
-    url: 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=Apple+iPhone+&_sacat=0',
-    disabled: true
+    url: 'https://www.ebay.com/b/Apple-iPhone/9355/bn_319682',
+    
 
 }
